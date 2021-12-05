6ee1e84539fe0c309f5f6ad509ead401b117e970
Hyeseon Ko <hsko0926@gmail.com>
2021-12-05 22:09:55 +0900

    Update palindrome-number.py

--- palindrome-number/palindrome-number.py
+++ palindrome-number/palindrome-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
-        print(x[0])
         for i in range(int(len(x)/2)):
             if x[i]!=x[-i-1]:
                 return False
