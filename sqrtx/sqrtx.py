98b98288f44b8d00dc7d93bc6dd36437fd4854d9
Hyeseon Ko <hsko0926@gmail.com>
2021-12-06 07:49:28 +0900

    Time: 4244 ms (5.00%), Space: 14.1 MB (70.60%) - LeetHub

---
+++ sqrtx/sqrtx.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # solution 1. go thourgh all numbers
+        i=0
+        while(1):
+            if x>=i*i and x<(i+1)*(i+1):
+                return i
+            i+=1
+        
