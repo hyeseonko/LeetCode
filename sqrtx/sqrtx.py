784d4576bc5226100b3474349d93a63ee53b1283
Hyeseon Ko <hsko0926@gmail.com>
2021-12-06 07:51:50 +0900

    Update sqrtx.py

--- sqrtx/sqrtx.py
+++ sqrtx/sqrtx.py
@@ -1,9 +1,11 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         # solution 1. go thourgh all numbers
-        i=0
-        while(1):
-            if x>=i*i and x<(i+1)*(i+1):
-                return i
-            i+=1
+        # i=0
+        # while(1):
+        #     if x>=i*i and x<(i+1)*(i+1):
+        #         return i
+        #     i+=1
+        # solution 2. sqrt
+        return int(sqrt(x))
         
