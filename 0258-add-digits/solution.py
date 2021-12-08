adeb66d29ec1f266deb5539e13e817a6698c4703
Hyeseon Ko <hsko0926@gmail.com>
2021-12-08 14:40:14 +0000

    solve Runtime - 28 ms (0.54%), Memory - 14.3 MB (100.00%)

---
+++ 0258-add-digits/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        output = [int(s) for s in str(num)]
+        while(1):
+            if len(output)==1:
+                return sum(output)
+            output = [int(s) for s in str(sum(output))]
+            
