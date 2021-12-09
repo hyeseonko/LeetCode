6e7e66b24597049821e3562fe9b2b1365592f908
Hyeseon Ko <hsko0926@gmail.com>
2021-12-09 23:35:21 +0900

    Time: 60 ms (58.97%), Space: 14.3 MB (91.10%) - LeetHub

---
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp=[cost[0], cost[1]] # dp:= stepped on ith step
+        for val in cost[2:]:
+            dp.append(val+min(dp[-1], dp[-2]))
+        return min(dp[-1],dp[-2])
+        
