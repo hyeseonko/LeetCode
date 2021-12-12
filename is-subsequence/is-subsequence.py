edf696e2b0a6cd4f31a512e8eada8d6cd51e02e4
Hyeseon Ko <hsko0926@gmail.com>
2021-12-12 16:15:27 +0900

    Time: 32 ms (72.07%), Space: 14.4 MB (24.57%) - LeetHub

---
+++ is-subsequence/is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        dp=[s[-i] for i in range(1,len(s)+1)]
+        
+        for each in t:
+            if len(dp)==0:
+                return True
+            if each==dp[-1]:
+                dp.pop()
+        if len(dp)==0:
+            return True
+        else:
+            return False
+        
