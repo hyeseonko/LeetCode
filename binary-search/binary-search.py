f542d7f08b413e64416d17a5eb1d53853d2ab78c
Hyeseon Ko <hsko0926@gmail.com>
2022-01-01 16:13:48 +0900

    Update binary-search.py

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -1,17 +1,16 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        
         start=0
         end = len(nums)-1
-        if nums[0]==target:
-            return 0
+        # always cursor is less than the end
+        if nums[-1]==target:
+            return end
+        
         while(start!=end):
             cur=int((start+end)/2)
             if nums[cur]==target:
                 return cur
-            elif nums[start]==target:
-                return start
-            elif nums[end]==target:
-                return end
             elif nums[cur]<target:
                 if start==cur:
                     break
@@ -21,4 +20,3 @@ class Solution:
                     break
                 end=cur
         return -1
-        
