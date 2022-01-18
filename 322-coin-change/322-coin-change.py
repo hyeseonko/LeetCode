2c41857d55b014eb96e0f766c91509ebc01a0b70
Hyeseon Ko <hsko0926@gmail.com>
2022-01-18 17:55:01 +0900

    Time: 1272 ms (82.70%), Space: 14.4 MB (91.80%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,16 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp=[0]
+        for i in range(1,amount+1):
+            if i in coins:
+                dp.append(1)
+            else:
+                candidate=[]
+                for coin in coins:
+                    if i-coin>0 and dp[i-coin]!=-1:
+                        candidate.append(dp[i-coin])
+                if not candidate:
+                    dp.append(-1)
+                else:
+                    dp.append(1+min(candidate))
+        return dp[-1]
