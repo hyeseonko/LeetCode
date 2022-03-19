44ed10ac7602e14f2c303b2d776206ae416ee6b6
Hyeseon Ko <hsko0926@gmail.com>
2022-03-19 14:46:28 +0900

    Delete two-sum directory
    update two-sum by new server

--- two-sum/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # solution1. O(n**2)
-        for i, num in enumerate(nums):
-            for j, num2 in enumerate(nums):
-                if num+num2==target and i!=j:
-                    return [i, j]
-            
-        
