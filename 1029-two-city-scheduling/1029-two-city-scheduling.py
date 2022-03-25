0abdae38e22502de9014df6e29f966dcfe208632
Hyeseon Ko <hsko0926@gmail.com>
2022-03-26 02:49:26 +0900

    Time: 74 ms (24.15%), Space: 14 MB (17.48%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,13 +1,15 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         base = [(cost[0], 1) if cost[0]<cost[1] else (cost[1], -1) for cost in costs]
-        basesum = sum([v for v, _ in base])
-        balanced = sum([b for _, b in base])       
+        basesum, balanced = 0, 0
+        for v, b in base:
+            basesum += v
+            balanced += b
         pass_over = balanced//2
         if pass_over==0:
             diff = 0
         elif pass_over>0:
-            diff = sum(sorted([abs(a-b) for a, b in costs if (a-b)*pass_over<0])[:abs(pass_over)])
+            diff = sum(sorted([abs(a-b) for a, b in costs if (a-b)<0])[:abs(pass_over)])
         else:
             diff = sum(sorted([abs(a-b) for a, b in costs if (a-b)>=0])[:abs(pass_over)])
         return basesum+diff
