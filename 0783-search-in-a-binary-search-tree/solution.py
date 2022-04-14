fe81877dfbefe30cca45098090d282c00b40109a
Hyeseon Ko <hsko0926@gmail.com>
2022-04-14 01:39:19 +0000

    solve Runtime - 93 ms (1.87%), Memory - 16.5 MB (100.00%)

---
+++ 0783-search-in-a-binary-search-tree/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        queue=[root]
+        while(queue):
+            cur = queue.pop(0)
+            if cur.val==val:
+                return cur
+            if cur.right:
+                queue.append(cur.right)
+            if cur.left:
+                queue.append(cur.left)
+        return None
+        
