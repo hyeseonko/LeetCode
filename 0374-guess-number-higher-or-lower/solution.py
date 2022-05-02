bdff75e00295be5fbf403b4bde3f5dc814c314ec
Hyeseon Ko <hsko0926@gmail.com>
2022-05-02 15:05:49 +0000

    solve Runtime - 33 ms (97.79%), Memory - 13.8 MB (100.00%)

---
+++ 0374-guess-number-higher-or-lower/solution.py
@@ -0,0 +1,21 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        start = 1
+        end = n
+        while(1):
+            cur = (start+end)//2
+            ans = guess(cur)
+            if ans == 0:
+                return cur
+            elif ans > 0:
+                start = cur+1
+            else:
+                end = cur-1
+        
