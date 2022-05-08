1ca2fb0b53f4bb2e93ff313a8dcf72ce0049230c
Hyeseon Ko <hsko0926@gmail.com>
2022-05-09 02:30:22 +0900

    Time: 193 ms (5.11%), Space: 14.3 MB (5.94%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        # (=1, )=-1
+        makingZeroIndices=[0,]
+        param=1
+        for i, each in enumerate(s[1:], 1):
+            param+=1 if each=="(" else -1
+            if param==0:
+                makingZeroIndices.extend([i, i+1])
+        makingZeroIndices=makingZeroIndices[:-1]
+        for index in makingZeroIndices[::-1]:
+            s=s[:index]+s[index+1:]
+        return s
+            
+            
