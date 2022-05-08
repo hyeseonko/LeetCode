8459a14e21aca53b6ae923db65389b8593bcf9b0
Hyeseon Ko <hsko0926@gmail.com>
2022-05-08 13:17:44 +0900

    Time: 3431 ms (33.33%), Space: 640.3 MB (16.67%) - LeetHub

---
+++ 2266-count-number-of-texts/2266-count-number-of-texts.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countTexts(self, pressedKeys: str) -> int:
+        dp=[1, 1]
+        for i, pkey in enumerate(pressedKeys[1:], 1):
+            temp=dp[-1]
+            if i>=1 and pressedKeys[i-1]==pkey:
+                temp+=dp[-2]
+                if i>=2 and pressedKeys[i-2]==pkey:
+                    temp+=dp[-3]
+                    if i>=3 and pkey in ["7", "9"] and pressedKeys[i-3]==pkey:
+                        temp+=dp[-4]
+            dp.append(temp)
+        return dp[-1]%1000000007
