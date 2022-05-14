955d0fd6391397e8070ab2bf3d7fd4950fd61463
Hyeseon Ko <hsko0926@gmail.com>
2022-05-14 21:25:32 +0900

    Time: 664 ms (62.84%), Space: 24.6 MB (81.05%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -4,11 +4,10 @@ class Solution:
         for num in nums[1:]:
             if num>dp[-1]:
                 dp.append(num)
-            else:
-                if len(dp)==1 or num<dp[-2]:
-                    dp[0]=num
-                elif num>dp[-2]:
-                    dp[1]=num
+            elif len(dp)==1 or num<dp[-2]:
+                dp[0]=num
+            elif num>dp[-2]:
+                dp[1]=num
             if len(dp)==3:
                 return True
         return False
