7cbeb9c441b3d9f98693fcab71d7c13d403e9a62
Hyeseon Ko <hsko0926@gmail.com>
2022-05-28 11:21:40 +0000

    solve Runtime - 169 ms (16.95%), Memory - 16.3 MB (100.00%)

--- 0268-missing-number/solution.py
+++ 0268-missing-number/solution.py
@@ -1,7 +1,3 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        maxnum = len(nums)
-        for i in range(maxnum+1):
-            if i not in nums:
-                return i
-        
+        return list(set(range(len(nums)+1))-set(nums))[0]
