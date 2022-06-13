de1b9bb03b913a8da54a4e0784952017c54ef824
Hyeseon Ko <hsko0926@gmail.com>
2022-06-13 14:48:07 +0000

    solve Runtime - 105 ms (35.57%), Memory - 15.3 MB (100.00%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,6 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, num in enumerate(nums[:-1]):
-            for j in range(i+1, len(nums)):
-                if num+nums[j]==target:
-                    return [i,j]
+        # O(n^2)
+        # for i, num in enumerate(nums[:-1]):
+        #     for j in range(i+1, len(nums)):
+        #         if num+nums[j]==target:
+        #             return [i,j]
+        
+        # O(n)
+        waiting_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in waiting_dict:
+                return [waiting_dict[nums[i]], i]
+            else:
+                waiting_dict[target-nums[i]]=i
