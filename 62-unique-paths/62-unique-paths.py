c78eaa69cc6b0e34b3e643a74ab06b3c7342613b
Hyeseon Ko <hsko0926@gmail.com>
2022-06-14 00:15:03 +0900

    Time: 56 ms (23.08%), Space: 13.9 MB (32.37%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,8 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # Combinatorics: (m-1)+(n-1) C min(m-1,n-1)
         output=1
         bunmo=1
-        for i, bunja in enumerate(range(m+n-2, max(m-1,n-1),-1)):
+        for i, bunja in enumerate(range(m+n-2, max(m,n)-1,-1), 1):
             output*=bunja
-            bunmo*=(i+1)
-        return int(output/bunmo)
+            bunmo*=i
+        return output//bunmo
