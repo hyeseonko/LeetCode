b5bd638c69f7f7e9252d210ac49c39f380cf7d02
Hyeseon Ko <hsko0926@gmail.com>
2022-06-14 14:44:25 +0000

    solve Runtime - 34 ms (2.33%), Memory - 13.8 MB (100.00%)

--- 0231-power-of-two/solution.py
+++ 0231-power-of-two/solution.py
@@ -1,10 +1,11 @@
+import math
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n<=0:
-            return False
+        return n and not (n & n - 1) # n <- added because of 0
 
-        import math
-        if str(math.log2(n)).split(".")[-1]=="0":
-            return True
-        else:
-            return False
+        # if n<=0:
+        #     return False
+        # if str(math.log2(n)).split(".")[-1]=="0":
+        #     return True
+        # else:
+        #     return False
