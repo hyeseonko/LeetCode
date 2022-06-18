a277210c11ba542b3ada4a5a229d56f568ac8c12
Hyeseon Ko <hsko0926@gmail.com>
2022-06-18 02:26:00 +0000

    solve Runtime - 38 ms (1.16%), Memory - 14 MB (100.00%)

--- 0078-subsets/solution.py
+++ 0078-subsets/solution.py
@@ -1,6 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = [[]]
-        for num in nums:
-            result += [i + [num] for i in result]
-        return result
+        # method 1. cascading
+        # output = [[]]
+        # for num in nums:
+        #     output += [o + [num] for o in output]
+        # return output
+        
+        # method 2. backtracking
+        def backtrack(first=0, curr=[]):
+            output.append(curr[:])
+            for i in range(first, n):
+                # step 1. add nums[i] to curr
+                curr.append(nums[i])
+                # step 2. go next
+                backtrack(i+1, curr)
+                # step 3. backtrack
+                curr.pop()
+        output = []
+        n = len(nums)
+        backtrack()
+        return output
