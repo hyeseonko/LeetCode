1d94877d3755c8e6c01236e52880de015a58995d
Hyeseon Ko <hsko0926@gmail.com>
2022-06-18 17:03:24 +0900

    Time: 117 ms (88.39%), Space: 15.8 MB (84.69%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,18 +1,21 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        # method 1. backtrack (recursive)
+        # def backtrack(path, index):
+        #     if len(path)==k:
+        #         output.append(path[:])
+        #         return
+        #     for i in range(index, n+1):
+        #         # add
+        #         path.append(i)
+        #         # go next
+        #         backtrack(path, i+1)
+        #         # backtrack
+        #         path.pop()
+        # output = []
+        # backtrack([], 1)
+        # return output
         
-        def backtrack(path, index):
-            if len(path)==k:
-                output.append(path[:])
-                return
-            for i in range(index, n+1):
-                # add
-                path.append(i)
-                # go next
-                backtrack(path, i+1)
-                # backtrack
-                path.pop()
-
-        output = []
-        backtrack([], 1)
-        return output
+        # method 2. python function
+        from itertools import combinations
+        return list(combinations(range(1, n+1), k))
