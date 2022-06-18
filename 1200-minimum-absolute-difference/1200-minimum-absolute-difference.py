55355f32819ad0de9960892c014321c214cf4671
Hyeseon Ko <hsko0926@gmail.com>
2022-06-18 19:55:52 +0900

    Time: 517 ms (42.60%), Space: 28.8 MB (49.31%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -1,12 +1,17 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        sarr = sorted(arr)
-        minval = sarr[1]-sarr[0]
-        output=[[sarr[0], sarr[1]]]
-        for i in range(2, len(arr)):
-            if sarr[i]-sarr[i-1] < minval:
-                minval = sarr[i]-sarr[i-1]
-                output = [[sarr[i-1], sarr[i]]]
-            elif sarr[i]-sarr[i-1]==minval:
-                output.append([sarr[i-1], sarr[i]])
-        return output
+        
+        arr.sort()
+        mn = min(b-a for a, b in zip(arr, arr[1:]))
+        return [[a, b] for a, b in zip(arr, arr[1:]) if b-a==mn]        
+  
+        # sarr = sorted(arr)
+        # minval = sarr[1]-sarr[0]
+        # output=[[sarr[0], sarr[1]]]
+        # for i in range(2, len(arr)):
+        #     if sarr[i]-sarr[i-1] < minval:
+        #         minval = sarr[i]-sarr[i-1]
+        #         output = [[sarr[i-1], sarr[i]]]
+        #     elif sarr[i]-sarr[i-1]==minval:
+        #         output.append([sarr[i-1], sarr[i]])
+        # return output
