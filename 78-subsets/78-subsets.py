e8fa837864d29672905aee1ac771546bd336d1ef
Hyeseon Ko <hsko0926@gmail.com>
2022-06-18 11:20:54 +0900

    Time: 35 ms (91.05%), Space: 14.1 MB (80.50%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,9 +1,26 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         # method 1. cascading
-        output = [[]]
+        # output = [[]]
+        # for num in nums:
+        #     output += [o + [num] for o in output]
+        # return output
         
-        for num in nums:
-            output += [o + [num] for o in output]
-        
+        # method 2. backtracking
+        def backtrack(first=0, curr=[]):
+            if len(curr)==k:
+                output.append(curr[:])
+                return
+            for i in range(first, n):
+                # step 1. add nums[i] to curr
+                curr.append(nums[i])
+                # step 2. go next
+                backtrack(i+1, curr)
+                # step 3. backtrack
+                curr.pop()
+        output = []
+        n = len(nums)
+        for k in range(n+1):
+            backtrack()
+            
         return output
