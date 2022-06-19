28350586c3eea28b728d6082ae320740a9563dae
Hyeseon Ko <hsko0926@gmail.com>
2022-06-19 18:49:59 +0900

    Time: 92 ms (81.36%), Space: 14 MB (16.64%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -1,3 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        return str(round(math.log(n, 3), 10)).split(".")[-1]=='0' if n>0 else False
+        # log
+        # return str(round(math.log(n, 3), 10)).split(".")[-1]=='0' if n>0 else False
+        
+        # number theory
+        return n>0 and not 3**19 % n
