efcdedd05780345f485d4d404a258b00d0ed533e
Hyeseon Ko <hsko0926@gmail.com>
2022-06-20 07:19:32 +0900

    Time: 314 ms (90.60%), Space: 66.4 MB (44.95%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,18 +1,22 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        # BFS
-        queue = [start]
-        length = len(arr)
-        while queue:
-            cur = queue.pop(0)
-            if arr[cur]==0:
-                return True
-
-            if cur + arr[cur] in range(length) and arr[cur+arr[cur]]!=-1:
-                queue.append(cur+arr[cur])
-            if cur - arr[cur] in range(length) and arr[cur-arr[cur]]!=-1:
-                queue.append(cur-arr[cur])           
-            
-            arr[cur] = -1
-            
+        
+        # method 2. Recursive
+        if 0<=start<len(arr) and arr[start]>=0:
+            arr[start]=-arr[start] # visited flag
+            return arr[start]==0 or self.canReach(arr, start+arr[start]) or self.canReach(arr, start-arr[start])
         return False
+        
+        # method 1. BFS
+        # queue = [start]
+        # length = len(arr)
+        # while queue:
+        #     cur = queue.pop(0)
+        #     if arr[cur]==0:
+        #         return True
+        #     if cur + arr[cur] in range(length) and arr[cur+arr[cur]]!=-1:
+        #         queue.append(cur+arr[cur])
+        #     if cur - arr[cur] in range(length) and arr[cur-arr[cur]]!=-1:
+        #         queue.append(cur-arr[cur])           
+        #     arr[cur] = -1
+        # return False
