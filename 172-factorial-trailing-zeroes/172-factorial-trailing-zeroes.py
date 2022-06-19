20be255898b868ea9b714e5213f828a6839a1f3d
Hyeseon Ko <hsko0926@gmail.com>
2022-06-20 07:38:56 +0900

    Time: 36 ms (87.58%), Space: 13.8 MB (58.05%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
@@ -1,9 +1,22 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        dp = [0,0,0,0,0]
-        for num in range(5, n+1):
-            if num%5==0:
-                dp.append(num//5 + dp[num//5])
-            else:
-                dp.append(dp[-1])
-        return dp[n]
+        
+        # method 2. Recursive O(logN)
+        output = 0
+        x = 5
+        while x<=n:
+            output += n//x
+            x *= 5
+        return output
+        
+        
+        return output
+        
+        # method 1. DP O(N)
+        # dp = [0,0,0,0,0]
+        # for num in range(5, n+1):
+        #     if num%5==0:
+        #         dp.append(num//5 + dp[num//5])
+        #     else:
+        #         dp.append(dp[-1])
+        # return dp[n]
