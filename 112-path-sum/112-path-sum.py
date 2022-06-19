d49c20f5a12915e9e8dca54033a477eedd6307a9
Hyeseon Ko <hsko0926@gmail.com>
2022-06-19 17:52:21 +0900

    Time: 62 ms (50.67%), Space: 15.1 MB (15.38%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -6,15 +6,23 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # method 1. BFS (queue)
+        # if not root:
+        #     return False
+        # queue=[(root, root.val)]
+        # while(queue):
+        #     cur, val = queue.pop(0)
+        #     if not cur.left and not cur.right and val == targetSum:
+        #         return True
+        #     if cur.left:
+        #         queue.append((cur.left, val+cur.left.val))
+        #     if cur.right:
+        #         queue.append((cur.right, val+cur.right.val))
+        # return False
+
+        # method 2. DFS (recursive)
         if not root:
             return False
-        queue=[(root, root.val)]
-        while(queue):
-            cur, val = queue.pop(0)
-            if not cur.left and not cur.right and val == targetSum:
-                return True
-            if cur.left:
-                queue.append((cur.left, val+cur.left.val))
-            if cur.right:
-                queue.append((cur.right, val+cur.right.val))
-        return False
+        if not root.left and not root.right and root.val==targetSum:
+            return True
+        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
