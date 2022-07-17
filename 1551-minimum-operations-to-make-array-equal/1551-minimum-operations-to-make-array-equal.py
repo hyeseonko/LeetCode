383a32876450935728aab9852c9f891c34e75942
Hyeseon Ko <hsko0926@gmail.com>
2022-07-17 17:28:05 +0900

    Time: 53 ms (72.53%), Space: 13.8 MB (66.21%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -1,3 +1,4 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        return (n//2)**2 if n%2==0 else ((n+1)//2)**2-((n+1)//2)
+        # return (n//2)**2 if n%2==0 else ((n+1)//2)**2-((n+1)//2)
+        return n**2//4
