4a454368daff9d0f13ba0511ab0a54a998057fd0
Hyeseon Ko <hsko0926@gmail.com>
2022-07-22 08:45:29 +0000

    solve Runtime - 62 ms (0.80%), Memory - 14 MB (100.00%)

---
+++ 0086-partition-list/solution.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        
+        h1 = l1 = ListNode(0) # h1: fixed, l1: moving
+        h2 = l2 = ListNode(0) # h2: fixed, l2: moving
+        while head:
+            if head.val < x:
+                l1.next = head
+                l1 = l1.next
+            else:
+                l2.next = head
+                l2 = l2.next
+            head = head.next
+        l2.next = None
+        l1.next = h2.next
+        return h1.next
