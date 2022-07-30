1b140358657d39fae4aa40dac08a672df81dc80c
Hyeseon Ko <hsko0926@gmail.com>
2022-07-30 21:39:02 +0900

    Time: 68 ms (86.19%), Space: 13.8 MB (78.41%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -4,7 +4,5 @@ class Solution:
         output = 0
         for cur_x, cur_y in points[1:]:
             output+=max(abs(cur_x-prev_x), abs(cur_y-prev_y))
-            prev_x = cur_x
-            prev_y = cur_y
+            prev_x, prev_y = cur_x, cur_y
         return output
-        
