5456e98bf14589b17b1f58280f8f626229475197
Hyeseon Ko <hsko0926@gmail.com>
2022-08-14 11:03:01 +0000

    solve Runtime - 1286 ms (96.23%), Memory - 0.0B (100.00%)

---
+++ 0607-sales-person/solution.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select name
+from SalesPerson
+where sales_id not in (select sales_id from Orders left join Company on Orders.com_id=Company.com_id where Company.name='RED')
