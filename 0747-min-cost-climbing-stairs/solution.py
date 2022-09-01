26f75c812bfd1df379e3fe09ea229f08591d14c7
Hyeseon Ko <hsko0926@gmail.com>
2022-09-01 03:57:20 +0000

    solve Runtime - 113 ms (6.53%), Memory - 13.9 MB (100.00%)

--- 0747-min-cost-climbing-stairs/solution.py
+++ 0747-min-cost-climbing-stairs/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp=[cost[0], cost[1]] # dp:= stepped on ith step
-        for val in cost[2:]:
-            dp.append(val+min(dp[-1], dp[-2]))
+        dp=[cost[0], cost[1]]
+        for i in range(2, len(cost)):
+            dp.append(cost[i]+min(dp[-1], dp[-2]))
         return min(dp[-1],dp[-2])
