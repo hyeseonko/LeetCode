565d7373dde501d119d0e455d5532e9767781778
Hyeseon Ko <hsko0926@gmail.com>
2022-09-06 14:25:45 +0000

    solve Runtime - 1233 ms (5.02%), Memory - 15.6 MB (100.00%)

--- 0283-move-zeroes/solution.py
+++ 0283-move-zeroes/solution.py
@@ -3,12 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        idx=0
-        zeros = nums.count(0)
-        nonzeros = len(nums) - zeros
-        while(idx<nonzeros):
-            if nums[idx]==0:
-                nums.pop(idx)
-                idx-=1
+        for num in nums:
+            if num==0:
+                nums.remove(0)
                 nums.append(0)
-            idx+=1                
+        return nums
