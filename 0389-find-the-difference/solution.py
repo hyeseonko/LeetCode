9f903650c7aa37c6355f32f184a53e6ad299991e
Hyeseon Ko <hsko0926@gmail.com>
2022-09-07 13:47:18 +0000

    solve Runtime - 58 ms (5.64%), Memory - 13.9 MB (100.00%)

--- 0389-find-the-difference/solution.py
+++ 0389-find-the-difference/solution.py
@@ -1,8 +1,10 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        t_list=[each for each in t]
-        for each in s:
-            if each in t_list:
-                t_list.remove(each)
-        return t_list[0]
-        
+        t_dict=dict()
+        for each in t:
+            if each not in t_dict:
+                t_dict[each]=0
+            t_dict[each]+=1
+        for c in t_dict:
+            if s.count(c)!=t_dict[c]:
+                return c
