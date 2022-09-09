06ae6d1f260b86df8f88b349521b2712ff4b21dd
Hyeseon Ko <hsko0926@gmail.com>
2022-09-09 09:17:17 +0000

    solve Runtime - 67 ms (0.05%), Memory - 14.2 MB (100.00%)

--- 0404-sum-of-left-leaves/solution.py
+++ 0404-sum-of-left-leaves/solution.py
@@ -6,15 +6,14 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        stack=[(root, 1)]
-        output=0
-        while(stack):
-            node, where = stack.pop(0)
-            if where==0 and not node.left and not node.right:
-                output+=node.val
-            if node.left:
-                stack.append((node.left, 0))
-            if node.right:
-                stack.append((node.right, 1))
+        q = [(root, 0)]
+        output = 0
+        while q:
+            cur, where = q.pop(0)
+            if not cur.left and not cur.right and where==-1:
+                output+=cur.val
+            if cur.left:
+                q.append((cur.left, -1))
+            if cur.right:
+                q.append((cur.right, 1))
         return output
-        
