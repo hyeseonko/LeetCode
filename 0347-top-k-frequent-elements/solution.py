0c5e1549b4cdf55c81da0748913daf5d55546b21
Hyeseon Ko <hsko0926@gmail.com>
2026-08-20 14:08:55 +0000

    solve Runtime - 0 ms (100.00%), Memory - 22.5 MB (99.22%)

--- 0347-top-k-frequent-elements/solution.py
+++ 0347-top-k-frequent-elements/solution.py
@@ -1,5 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        output = [(num, nums.count(num)) for num in set(nums)]
-        s_output = sorted(output, key=lambda x: x[1], reverse=True)
-        return list(map(lambda x: x[0], s_output))[:k]
+        cnt = {}
+        for num in nums:
+            cnt[num] = cnt.get(num, 0)+1
+        
+        sorted_cnt = sorted(cnt, key=cnt.get, reverse=True)
+        return sorted_cnt[:k]
