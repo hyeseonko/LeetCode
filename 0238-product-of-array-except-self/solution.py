a47f270cc8e4ac8484b17dcbd98b337e6d38b8fe
Hyeseon Ko <hsko0926@gmail.com>
2026-08-30 22:23:59 +0000

    solve Runtime - 31 ms (33.08%), Memory - 25.3 MB (81.03%)

--- 0238-product-of-array-except-self/solution.py
+++ 0238-product-of-array-except-self/solution.py
@@ -1,23 +1,10 @@
-import math
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        zero_counts = nums.count(0)
-
-        # case 1. the number of '0's >= 2
-        if zero_counts>=2:
-            return [0]*len(nums)
+        answer, suffix, prefix = [1]*len(nums), 1, 1
+        for i in range(len(nums)):
+            answer[i]*=prefix
+            prefix*=nums[i]
+            answer[-i-1]*=suffix
+            suffix*=nums[-i-1]
         
-        # case 2. the number of '0's == 1
-        elif zero_counts==1:
-            zero_index = nums.index(0)
-            output = [0]*len(nums)
-            nums.remove(0)
-            output[zero_index] = math.prod(nums)
-            return output
-        
-        # case 2. the number of '0's == 0
-        else:
-            prod = math.prod(nums)
-            output = [prod//num for num in nums]
-            return output
+        return answer
