3bc017b83e6a8a79876cde7a3584d8540aa21cb1
Hyeseon Ko <hsko0926@gmail.com>
2026-08-31 22:50:40 +0000

    solve Runtime - 7 ms (81.15%), Memory - 20.2 MB (26.19%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        filtered_s = [c.lower() for c in s if c.isalnum()]
-        half_len = len(filtered_s) // 2
+        filtered_s = [each for each in s if each.isalnum()]
+        half_len = len(filtered_s)//2
         for i in range(half_len):
-            if filtered_s[i] != filtered_s[-i - 1]:
+            if filtered_s[i].lower()!=filtered_s[-i-1].lower():
                 return False
         return True
+
